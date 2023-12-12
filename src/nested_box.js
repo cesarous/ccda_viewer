e30ec302d90de271537f94ee3fb219443bbf78af
cesarous <cero@umich.edu>
2023-12-12 16:04:48 -0600

    Update nested_box.js

--- src/nested_box.js
+++ src/nested_box.js
@@ -1,15 +1,45 @@
-// NestedBox.js
 import React from 'react';
 import styled from 'styled-components';
 
 const StyledNestedBox = styled.div`
-  background-color: #f9f9f9; /* Light gray background color */
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  padding: 15px;
+  margin-top: 15px;
+
+  .nested-box {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .box-container {
+    background-color: #f9f9f9;
+    margin-bottom: 20px;
+  }
+
+  .box {
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+
+  .key {
+    font-weight: bold;
+    text-align: left;
+  }
+
+  .value {
+    margin-top: 5px;
+  }
+`;
+
+const StyledSecondLevelBox = styled.div`
+  background-color: #f9f9f9;
   border: 1px solid #7fbfff;
   border-radius: 8px;
   padding: 15px;
   margin-top: 15px;
 
-  .box-container {
+  .second-level-box-container {
     display: flex;
     flex-wrap: wrap;
   }
@@ -28,6 +58,47 @@ const StyledNestedBox = styled.div`
   }
 `;
 
+function SecondLevelBox({ data }) {
+  if (data === null || typeof data !== 'object') {
+    return String(data);
+  }
+
+  const entries = Object.entries(data || {});
+
+  if (entries.length === 1) {
+    const [key, value] = entries[0];
+    if (value === null || (typeof value === 'string' && value.trim() === '')) {
+      return null;
+    }
+    return <StyledSecondLevelBox data={value} />;
+  }
+
+  return (
+    <StyledSecondLevelBox>
+      <div className='second-level-box-container'>
+        {Object.entries(data || {}).map(([key, value], index) => {
+          if (value === null || (typeof value === 'string' && value.trim() === '')) {
+            return null;
+          }
+          const hasValidChildren = Object.keys(value).length > 0;
+
+          return (
+            <div key={index} className="second-level-box-container">
+              <div className="box">
+                <div className="key">{String(key)}:</div>
+                {hasValidChildren ? (
+                  <SecondLevelBox data={value}></SecondLevelBox>
+                ) : (
+                  <div className="value">{String(value)}</div>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </StyledSecondLevelBox>
+  );
+}
 
 function NestedBox({ data }) {
   if (data === null || typeof data !== 'object') {
@@ -37,42 +108,37 @@ function NestedBox({ data }) {
   const entries = Object.entries(data || {});
 
   if (entries.length === 1) {
-    // If there's only one item, display it without the index
     const [key, value] = entries[0];
     if (value === null || (typeof value === 'string' && value.trim() === '')) {
       return null;
     }
-    return <NestedBox data={value} />;
+    return <StyledSecondLevelBox data={value} />;
   }
 
   return (
     <StyledNestedBox>
+      <div className="nested-box">
+        {Object.entries(data || {}).map(([key, value], index) => {
+          if (value === null || typeof value === 'string') {
+            return null;
+          }
+          const hasValidChildren = Object.keys(value).length > 0;
 
-    <div className="nested-box">
-      {Object.entries(data || {}).map(([key, value], index) => {
-        // Check if the value is null, and return null if it is
-        if (value === null || (typeof value === 'string' && value.trim() === '')) {
-          return null;
-        }
-        // Check if the object has any properties
-        const hasValidChildren = Object.keys(value).length > 0;
-
-        return (
-          <div key={index} className="box-container">
-            <div className="box">
-              <div className="key">{String(key)}:</div>
-              {hasValidChildren ? (
-                <NestedBox data={value}></NestedBox>
-              ) : (
-                <div className="value">{String(value)}</div>
-              )}
+          return (
+            <div key={index} className="box-container">
+              <div className="box">
+                <div className="key">{String(key)}:</div>
+                {hasValidChildren ? (
+                  <SecondLevelBox data={value}></SecondLevelBox>
+                ) : (
+                  <div className="value">{String(value)}</div>
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </StyledNestedBox>
-
   );
 }
 
